be8ad2a0b8a25fe677f2c34d01d5890d5fcc58d0
Paul Nikitochkin <pftg@users.noreply.github.com>
2019-09-07 11:32:29 +0300

    setup prettier

--- install/.eslintrc.js
+++ install/.eslintrc.js
@@ -1,14 +1,16 @@
 // Documentation on http://eslint.org/docs/rules/
 
 module.exports = {
-  "extends": "airbnb-base",
+  "extends": ["airbnb-base", "prettier"],
+  "plugins": ["prettier"],
   "env": {
     "browser": true,
     "node": true,
-    "jquery": true,
+    "jquery": true
   },
   "rules": {
     "no-unused-expressions": ["error", { "allowShortCircuit": true }],
     "max-len": ["error", { "code": 113, "tabWidth": 2 }],
+    "prettier/prettier": ["error"]
   },
 };

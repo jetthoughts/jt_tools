9a5641577e183fbba1553b14a1a268e9b687596f
Paul Nikitochkin <pftg@users.noreply.github.com>
2019-12-14 10:04:30 +0200

    fix tests

--- template.rb
+++ template.rb
@@ -104,7 +104,8 @@ say '=> Set git hooks'
 run 'git config core.hooksPath ./bin/git-hooks'
 
 say '=> Install all new dependencies'
-run 'hash brew 2>/dev/null && (brew bundle check || brew bundle install)'
+pp run 'hash brew 2> /dev/null || echo "No Brew"'
+run 'hash brew 2> /dev/null && (brew bundle check || brew bundle install)'
 
 after_bundle do
   say '=> Setup default bundle config'
